94f3aabda63638d1667e4fb67a91df962b9d26e8
GSB <g_s_b@mail.ru>
2019-01-12 11:59:17 +0900

    Добавил ссылку

--- views/site/index.php
+++ views/site/index.php
@@ -8,16 +8,35 @@ $this->title = 'Сервис';
 
 <div class="jumbotron">
     <div class="row">
-        <div class="alert alert-success cur col-xs-5">
-            <i class="glyphicon glyphicon-list-alt gi-4x"> </i> <br> Конструктор бланков 
+        <a href="#">
+        <div class="alert alert-success cur shadow col-xs-5">
+            <i class="glyphicon glyphicon-list-alt gi-4x"> </i> <br> Заявки 
         </div> 
+        </a>
         
         <div class="col-xs-1">
-            
         </div> 
-        <div class="alert alert-success cur col-xs-5">
-            <i class="glyphicon glyphicon-user gi-4x"> </i> <br> Пользователи 
-        </div> 
-
+        <a href="#">
+        <div class="alert alert-success cur shadow col-xs-5">
+            <i class="glyphicon glyphicon-list gi-4x"> </i> <br> Клиенты 
+        </div>
+        </a> 
     </div>
+    
+    <div class="row">
+        <a href="#">
+        <div class="alert alert-success cur shadow col-xs-5">
+            <i class="glyphicon glyphicon-credit-card gi-4x"> </i> <br> Конструктор бланков 
+        </div>
+        </a> 
+        
+        <div class="col-xs-1">
+        </div> 
+        <a href="#">
+        <div class="alert alert-success cur shadow col-xs-5">
+            <i class="glyphicon glyphicon-user gi-4x"> </i> <br> Пользователи 
+        </div>
+        </a> 
+    </div>
+
 </div>
